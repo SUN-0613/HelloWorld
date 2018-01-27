f5c2c50702086169b9c2ae1f6d4df4330a0df69d
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-27 17:37:51 +0900

    VB:UWP MVVMにてHello World

---
+++ VB_UWP/HelloWorld/CViewModel.vb
@@ -0,0 +1,82 @@
+﻿Imports System.ComponentModel
+
+''' <summary>
+''' MVVM:View Model
+''' </summary>
+Public Class CViewModel
+
+    Implements INotifyPropertyChanged
+    Implements IDisposable
+
+    ''' <summary>
+    ''' プロパティ変更イベント
+    ''' </summary>
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    ''' <summary>
+    ''' プロパティネーム
+    ''' </summary>
+    Private Const ProName As String = "Value"
+
+    ''' <summary>
+    ''' 重複呼出FLG
+    ''' </summary>
+    Private disposedValue As Boolean
+
+    ''' <summary>
+    ''' 値
+    ''' </summary>
+    Private _Value As String
+
+    ''' <summary>
+    ''' 新規作成
+    ''' </summary>
+    Public Sub New()
+        Me._Value = ""
+    End Sub
+
+    ''' <summary>
+    ''' 新規作成
+    ''' </summary>
+    ''' <param name="Str">初期値</param>
+    Public Sub New(Str As String)
+        Me._Value = Str
+    End Sub
+
+    ''' <summary>
+    ''' 終了処理
+    ''' </summary>
+    ''' <param name="disposing">マネージ破棄FLG</param>
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: マネージ状態を破棄します (マネージ オブジェクト)。
+            End If
+        End If
+        disposedValue = True
+    End Sub
+
+    ''' <summary>
+    ''' 終了処理
+    ''' </summary>
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+    End Sub
+
+    ''' <summary>
+    ''' 値の取得・更新
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Value() As String
+        Get
+            Return Me._Value
+        End Get
+        Set(value As String)
+            If Not value.Equals(Me._Value) Then
+                Me._Value = value
+                RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(ProName))
+            End If
+        End Set
+    End Property
+
+End Class
---
+++ VB_UWP/HelloWorld/MainPage.xaml.vb
@@ -0,0 +1,25 @@
+﻿' 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+''' <summary>
+''' それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    ''' <summary>
+    ''' ViewModelプロパティ定義
+    ''' </summary>
+    Public Property VModel As New CViewModel()
+
+    ''' <summary>
+    ''' 新規作成
+    ''' </summary>
+    Public Sub New()
+
+        InitializeComponent()
+
+        VModel.Value = "Hello World!"
+
+    End Sub
+
+End Class
